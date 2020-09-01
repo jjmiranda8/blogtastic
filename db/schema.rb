a646159599e06147efb5712003a5201a8164023c
jjmiranda8 <jmlivingfree@gmail.com>
2020-09-01 14:01:53 -0500

    Added category model with validations for name. Ran migration.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_28_213135) do
+ActiveRecord::Schema.define(version: 2020_09_01_183850) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "adminpack"
@@ -22,6 +22,12 @@ ActiveRecord::Schema.define(version: 2020_08_28_213135) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer "user_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
